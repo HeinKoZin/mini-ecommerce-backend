2fa49564374b3aea25cde8f0c1bf5e3abaffd9b7
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-05 16:33:52 +0630

    Generate fake product data.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,22 @@
-import { PrismaClient, Shop, User, UserType } from '@prisma/client';
+import {
+  PrismaClient,
+  Product,
+  ProductStatus,
+  Shop,
+  User,
+  UserType,
+} from '@prisma/client';
 import { faker } from '@faker-js/faker';
 
 type InputUserType = Omit<User, 'id' | 'createdAt' | 'updatedAt'>;
 type InputShopType = Omit<Shop, 'id' | 'createdAt' | 'updatedAt'>;
+type InputProductType = Omit<Product, 'id' | 'createdAt' | 'updatedAt'>;
 
 // generate fake data of a user
 const generateUser = () => {
   return {
     name: faker.name.findName(),
-    email: faker.internet.email(),
+    email: faker.internet.email().toLowerCase(),
     password: faker.internet.password(),
     role: faker.random.arrayElement([UserType.USER]),
   };
@@ -19,7 +27,29 @@ const generateShop = () => {
   return {
     name: faker.company.companyName(),
     address: faker.address.streetAddress(),
-    phone: faker.phone.phoneNumber(),
+    phone: faker.phone.phoneNumber('+95 ## ### ####'),
+    // generate unique company name without spaces
+    shopname: faker.company
+      .companyName()
+      .toLowerCase()
+      .replace(',', '')
+      .replace(/\s/g, ''),
+  };
+};
+
+// generate fake data of a product
+const generateProduct = () => {
+  return {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    quantity: faker.datatype.number({ max: 25 }),
+    product_code: faker.random.alphaNumeric(10).toUpperCase(),
+    status: faker.random.arrayElement([
+      ProductStatus.AVAILABLE,
+      ProductStatus.STOCK_OUT,
+      ProductStatus.PRE_ORDER,
+    ]),
+    shopId: faker.datatype.number({ min: 1, max: 100 }),
   };
 };
 
@@ -54,11 +84,13 @@ const seedShops = async () => {
       name: 'Kay Kay',
       address: 'No.253, St.51, Botahtaung',
       phone: '0941097449',
+      shopname: 'kaykay',
     },
     {
       name: 'Hein Hein',
       address: 'No.253, St.51, Botahtaung',
       phone: '09950668891',
+      shopname: 'heinhein',
     },
   ];
 
@@ -66,6 +98,20 @@ const seedShops = async () => {
     shops.push(generateShop());
   }
   await prisma.shop.createMany({ data: shops, skipDuplicates: true });
+
+  const isShopExist = prisma.shop.findMany({});
+
+  isShopExist ? seedProducts() : console.log('Shop not found');
+};
+
+const seedProducts = async () => {
+  const prisma = new PrismaClient();
+  const products: InputProductType[] = [];
+
+  for (let i = 0; i < 100; i++) {
+    products.push(generateProduct());
+  }
+  await prisma.product.createMany({ data: products, skipDuplicates: true });
 };
 
 seedUsers();
