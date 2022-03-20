8866564e848cf3b3b87445c182091ef2388044ed
Hein Ko Zin <heinkozin4@gmail.com>
2022-03-21 00:42:11 +0630

    fix: Jwt token exp is changed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,7 +22,7 @@ import { JwtModule } from '@nestjs/jwt';
     }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+      signOptions: { expiresIn: '7d' },
     }),
     TestModule,
     UsersModule,
